9733e06d4d6642330fd5b086374a7478e12d1540
Victor Coutinho Vieira Santos <69547580+ViktorCVS@users.noreply.github.com>
2021-04-29 19:29:37 -0300

    Update getting_data.py

--- getting_data.py
+++ getting_data.py
@@ -1,6 +1,9 @@
-import serial
+import serial #Importing library to get data from serial
 
-arduino = serial.Serial("COM3",9600)
+arduino = serial.Serial("COM3",9600) #Getting data from "COM3" port with 9600 Bald Rate (we set it in .ino code)
 
 while True:
-    print(arduino.readline())
+    
+    #Infinity loop to get data in real time without a stop condition
+    
+    print(arduino.readline())  #printing in terminal the result
