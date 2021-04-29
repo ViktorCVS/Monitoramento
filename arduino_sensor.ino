cc2729b4146dd3277835c0874d58f100e5995511
Victor Coutinho Vieira Santos <69547580+ViktorCVS@users.noreply.github.com>
2021-04-29 19:23:22 -0300

    Update arduino_sensor.ino

--- arduino_sensor.ino
+++ arduino_sensor.ino
@@ -1,29 +1,26 @@
-#include <dht.h>
+#include <dht.h> #importing dht11 library
 
-#define    dht_pin    8   //pino de sinal do dht11 ligado no digital 5
+#define    dht_pin    8   //Setting pin 8 to get data from the sensor
 
 dht   my_dht;   //objeto para o sensor
 
-int    temperatura = 0x00,   //armazena a temperatura em inteiro
-       umidade     = 0x00;   //armazena a umidade em inteiro
+int    temperatura = 0x00,   //Store temperature as an integer
+       umidade     = 0x00;   //Store umidity as an integer
        
 void setup() { 
 
- Serial.begin(9600);
+ Serial.begin(9600); //Stating the serial output with 9600 Bald Rate
  
 }
 
 void loop() {
 
- my_dht.read11(dht_pin);
+ my_dht.read11(dht_pin); //In loop, reading each time the values from the sensor
 
- temperatura = my_dht.temperature;
- umidade     = my_dht.humidity;
-
- Serial.print(my_dht.temperature);
- Serial.print(",");
- Serial.println(my_dht.humidity);
- delay(1000);
+ Serial.print(my_dht.temperature); //Sending the temperature for serial
+ Serial.print(","); //Separating values with ',' to be easier to get this data in Python
+ Serial.println(my_dht.humidity); //Sending the umidity for serial
+ delay(1000); //Get data from sensor each 1 second
  
  
 }
